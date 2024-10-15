21d1e4eaa406d9d2d25fb3ba38ee0b7636059132
warmans <>
2024-10-15 22:43:06 +0100

    Removed old command

--- cmd/root.go
+++ cmd/root.go
@@ -4,7 +4,6 @@ import (
 	"github.com/spf13/cobra"
 	transcribe "github.com/warmans/tvgif/cmd/aisrt"
 	"github.com/warmans/tvgif/cmd/bot"
-	"github.com/warmans/tvgif/cmd/search"
 	"github.com/warmans/tvgif/cmd/tools"
 	"log/slog"
 )
@@ -22,7 +21,6 @@ func init() {
 
 // Execute executes the root command.
 func Execute(logger *slog.Logger) error {
-	rootCmd.AddCommand(search.NewSearchCommand(logger))
 	rootCmd.AddCommand(bot.NewBotCommand(logger))
 	rootCmd.AddCommand(tools.NewToolsCommand(logger))
 	rootCmd.AddCommand(transcribe.NewRootCommand(logger))
